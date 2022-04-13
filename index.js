275adb7949b02ab17dd285d349686a8d7c726683
Shivam <shivam.gup82@gmail.com>
2022-04-13 19:54:34 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+let button = document.getElementById("submitButton");
+console.log(button);
+button.addEventListener("click", function create(title, callback) {
+  // [START sheets_create]
+  console.log(gapi.client);
+});
